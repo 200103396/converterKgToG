765516f1807543cb1db7e07906e1889ce9309173
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-12 13:13:57 +0600

    main fun

--- app/src/main/java/com/example/converterapp/MainActivity.kt
+++ app/src/main/java/com/example/converterapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.converterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollB: Button = findViewById(R.id.button)
+        rollB.setOnClickListener { calc() }
     }
+
+    private fun calc() {
+   val num: Editable? = findViewById<EditText?>(R.id.dannyi_kg).text
+   val g = num.toString()
+        val tt = findViewById<TextView?>(R.id.textView)
+        val sum = g.toInt() * 1000
+        tt.text = "" + sum + " g in " + g + " kg"
+    }
+
 }
