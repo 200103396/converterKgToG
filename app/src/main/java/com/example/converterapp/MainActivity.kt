11efc3ef17ac9e3b9db30dc54d2deaba5b7cc354
200103396 <91262551+200103396@users.noreply.github.com>
2022-10-12 13:47:21 +0600

    converting to gramm

--- app/src/main/java/com/example/converterapp/MainActivity.kt
+++ app/src/main/java/com/example/converterapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,14 +15,26 @@ class MainActivity : AppCompatActivity() {
 
         val rollB: Button = findViewById(R.id.button)
         rollB.setOnClickListener { calc() }
+
+        val roll: Button = findViewById(R.id.buttong)
+        roll.setOnClickListener { calcg() }
     }
 
+
     private fun calc() {
-   val num: Editable? = findViewById<EditText?>(R.id.dannyi_kg).text
-   val g = num.toString()
+        val num: Editable? = findViewById<EditText?>(R.id.dannyi_kg).text
+        val g = num.toString()
         val tt = findViewById<TextView?>(R.id.textView)
         val sum = g.toInt() * 1000
         tt.text = "" + sum + " g in " + g + " kg"
     }
 
+
+    private fun calcg() {
+        val num: Editable? = findViewById<EditText?>(R.id.dannyi_gr).text
+        val g = num.toString()
+        val tt = findViewById<TextView?>(R.id.textView2)
+        val sum = g.toDouble() / 1000.0
+        tt.text = "" + sum + " Kg in " + g + " g"
+    }
 }
